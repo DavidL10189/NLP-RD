098a3980b9e28a8141ffad199379c41e28f4b9e5
DavidL10189 <dlawrence188843@troy.edu>
2024-06-23 20:12:05 +0000

    Updated UI to add processing response

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,13 +11,9 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 
-
-
 #Variables to hold our different documents to be used
 fileTroy = "RAGDocuments/prompt_answer.csv"
 fileOS = "RAGDocuments/prompt_OS_answer.csv"
-
-
 
 #Function to load a document
 def DocLoader(fileName):
@@ -29,6 +25,9 @@ def DocSplitter(document):
    splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=200)
    return splitter.split_documents(document)
 
+#Text to display status to the user
+headerDisplay = "Hello"
+detailDisplay = "Please ask a question above"
 
 #Load our documents used for RAG
 loadedTroy = DocLoader(fileTroy)
@@ -38,49 +37,27 @@ loadedOS = DocLoader(fileOS)
 troy_Split = DocSplitter(loadedTroy)
 OS_Split = DocSplitter(loadedOS)
 
-#CSVLoader()
-#ragDirLoader = DirectoryLoader("RAGDocuments", glob='*.csv"')
-#ragDocuments = ragDirLoader.load()
-
 #Get API Key from Secrets file into a variable
 apikey = st.secrets["API_KEY"]
 
 st.title("Troy and OS Interface chatbox")
 
 #Apply the Gemini API Key
-#ggi.configure(api_key = apikey)
 lcGemini = ChatGoogleGenerativeAI(model="gemini-pro", google_api_key=apikey)
 
-#Create instance of Google LLM and create chat object
-#geminiModel = ggi.GenerativeModel("gemini-1.5-pro")
-#geminiChat = geminiModel.start_chat()
+#Prompt the user to input their request
+userQuestion = st.text_area("You can ask about Troy University or about how to perform Windows OS tasks")
 
-#Prompt the user to input their request - will use enter in the box
-#instead of a button for the user to send the question
-userQuestion = st.text_input("You can ask the model about Troy University or about how to perform Windows OS tasks")
+responseTitle = st.empty()
+
+responseTitle.write("Please enter your question above")
+
 
 #Functionality to perform the communication with the API
 if userQuestion:
+   responseTitle.write("Processing")
    userQuestion_Prompt = PromptTemplate.from_template("{userQuestion}")
-   lcChain = LLMChain(llm=lcGemini, prompt = userQuestion, verbose=True)
+   lcChain = LLMChain(llm=lcGemini, prompt = userQuestion_Prompt, verbose=True)
    lcResponse = lcChain.run(userQuestion)
+   responseTitle.write("")
    st.write(lcResponse)
-    #Send the question to Gemini and get the response - use streamed respone
-    #for better UI responsiveness.
-    #geminiResponse = geminiChat.send_message(userQuestion,stream=True)
-      
-    #st.subheader("Gemini's Response: ")    
-    
-    #Use a temporary string to hold the response    
-    #responseComplete = ""
-    
-    #Piece the response together and only write it if it ends in a newline
-    #for responseChunk in geminiResponse:     
-    #     responseTemp = responseChunk.text
-    #     if "\n" in responseTemp: 
-    #        newlinePos = responseTemp.rfind('\n')
-    #        responseComplete += responseTemp[:newlinePos - 1]
-    #        st.write(responseComplete)
-    #        responseComplete = responseTemp[newlinePos:]                  
-    #     else:
-    #        responseComplete += responseTemp
