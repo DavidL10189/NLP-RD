2751db71758c390ef7934224bc19fb461fad2a66
DavidL10189 <dlawrence188843@troy.edu>
2024-06-23 19:42:18 +0000

    Langchain debugging

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,10 +7,17 @@ import google.generativeai as ggi
 from langchain_community.document_loaders import DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter, CharacterTextSplitter
 from langchain.document_loaders.csv_loader import CSVLoader
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+
+
 
 #Variables to hold our different documents to be used
 fileTroy = "RAGDocuments/prompt_answer.csv"
 fileOS = "RAGDocuments/prompt_OS_answer.csv"
+
+
 
 #Function to load a document
 def DocLoader(fileName):
@@ -41,11 +48,12 @@ apikey = st.secrets["API_KEY"]
 st.title("Troy and OS Interface chatbox")
 
 #Apply the Gemini API Key
-ggi.configure(api_key = apikey)
+#ggi.configure(api_key = apikey)
+lcGemini = ChatGoogleGenerativeAI(model="gemini-pro", google_api_key=apikey)
 
 #Create instance of Google LLM and create chat object
-geminiModel = ggi.GenerativeModel("gemini-1.5-pro")
-geminiChat = geminiModel.start_chat()
+#geminiModel = ggi.GenerativeModel("gemini-1.5-pro")
+#geminiChat = geminiModel.start_chat()
 
 #Prompt the user to input their request - will use enter in the box
 #instead of a button for the user to send the question
@@ -53,22 +61,26 @@ userQuestion = st.text_input("You can ask the model about Troy University or abo
 
 #Functionality to perform the communication with the API
 if userQuestion:
+   userQuestion_Prompt = PromptTemplate.from_template("{userQuestion}")
+   lcChain = LLMChain(llm=lcGemini, prompt = userQuestion, verbose=True)
+   lcResponse = lcChain.run(userQuestion)
+   st.write(lcResponse)
     #Send the question to Gemini and get the response - use streamed respone
     #for better UI responsiveness.
-    geminiResponse = geminiChat.send_message(userQuestion,stream=True)
+    #geminiResponse = geminiChat.send_message(userQuestion,stream=True)
       
-    st.subheader("Gemini's Response: ")    
+    #st.subheader("Gemini's Response: ")    
     
     #Use a temporary string to hold the response    
-    responseComplete = ""
+    #responseComplete = ""
     
     #Piece the response together and only write it if it ends in a newline
-    for responseChunk in geminiResponse:     
-         responseTemp = responseChunk.text
-         if "\n" in responseTemp: 
-            newlinePos = responseTemp.rfind('\n')
-            responseComplete += responseTemp[:newlinePos - 1]
-            st.write(responseComplete)
-            responseComplete = responseTemp[newlinePos:]                  
-         else:
-            responseComplete += responseTemp
+    #for responseChunk in geminiResponse:     
+    #     responseTemp = responseChunk.text
+    #     if "\n" in responseTemp: 
+    #        newlinePos = responseTemp.rfind('\n')
+    #        responseComplete += responseTemp[:newlinePos - 1]
+    #        st.write(responseComplete)
+    #        responseComplete = responseTemp[newlinePos:]                  
+    #     else:
+    #        responseComplete += responseTemp
