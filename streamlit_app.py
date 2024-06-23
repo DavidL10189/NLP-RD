fee6c0a5f2e286ecea45e2e19ba2fb095e0c8806
DavidL10189 <dlawrence188843@troy.edu>
2024-06-23 01:15:36 +0000

    Comment and code cleanup - no functional changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,34 +9,31 @@ import google.generativeai as ggi
 #Get API Key from Secrets file into a variable
 apikey = st.secrets["API_KEY"]
 
-#UI - Title
 st.title("Troy and OS Interface chatbox")
 
-#Apply our Gemini API Key
+#Apply the Gemini API Key
 ggi.configure(api_key = apikey)
 
-#Create instance of Google LLM
+#Create instance of Google LLM and create chat object
 geminiModel = ggi.GenerativeModel("gemini-1.5-pro")
-
-#Use factory to return chat object
 geminiChat = geminiModel.start_chat()
 
-#Prompt the user to input their request
+#Prompt the user to input their request - will use enter in the box
+#instead of a button for the user to send the question
 userQuestion = st.text_input("You can ask the model about Troy University or about how to perform Windows OS tasks")
-#submitButton = st.button("Send your question to Gemini")
 
 #Functionality to perform the communication with the API
 if userQuestion:
     #Send the question to Gemini and get the response
     geminiResponse = geminiChat.send_message(userQuestion,stream=True)
-    #Display the response in the page's UI
+      
     st.subheader("Gemini's Response: ")    
     
-    #Temporary string to hold the Gemini response. To make the output display smoother.
+    #Use a temporary string to hold the response    
     responseComplete = ""
     
-    #Loop through the sections of response text
-    for responseChunk in geminiResponse:
+    #Piece the response together and only write it if it ends in a newline
+    for responseChunk in geminiResponse:     
          responseTemp = responseChunk.text
          if "\n" in responseTemp: 
             newlinePos = responseTemp.rfind('\n')
